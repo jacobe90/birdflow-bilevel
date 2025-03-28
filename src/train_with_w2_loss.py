7de4dcaaf1736f6017a3d90a8c5ef49dd61baa5a
jacobe90 <jacobepstein90@gmail.com>
2025-03-28 19:18:05 +0000

    saving

--- src/train_with_w2_loss.py
+++ src/train_with_w2_loss.py
@@ -90,7 +90,7 @@ ent_weight = 1e-4
 dist_pow = 0.4
 dont_normalize = False
 learning_rate = 0.1
-training_steps = 600
+training_steps = 2100
 rng_seed = 42
 save_pkl = True
 weeks = 26
@@ -98,7 +98,7 @@ weeks = 26
 # parameters for epsilon schedulers
 start = 2
 final = 0.01
-decay_after = 500
+decay_after = 2000
 decay_iters = 100
 
 hdf_src = os.path.join(root, f'{species}_{ebirdst_year}_{resolution}km.hdf5')
